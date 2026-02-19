548159ee95a611cacb38c8647ab047413b0de02e
Kush Dubey <kush.dubey@sentry.io>
2026-02-18 17:34:52 -0800

    avoid thrashing, fail loudly

--- src/grouping_trainer/danger.py
+++ src/grouping_trainer/danger.py
@@ -1,3 +1,4 @@
+from typing import Callable
 import torch
 import torch.nn.functional as F
 
@@ -13,6 +14,8 @@ class SentenceTransformer(SentenceTransformerGT):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self._buckets = (64, 128, 256, 512, 1024, 2048)
+        # The vast majority of stacktraces are in this range.
+        self._compiled_forward: Callable[[dict[str, torch.Tensor]], dict[str, torch.Tensor]] | None = None
 
     def tokenize(self, texts: list[str], **kwargs) -> dict[str, torch.Tensor]:
         """
@@ -30,6 +33,9 @@ class SentenceTransformer(SentenceTransformerGT):
             num_padding_tokens = target_len - current_len
             pad_val = self.tokenizer.pad_token_id if self.tokenizer.pad_token_id is not None else 0
 
+            if extra_keys := (set(encodings.keys()) - {"input_ids", "attention_mask", "token_type_ids"}):
+                raise ValueError(f"Unexpected encoding keys: {extra_keys}")
+
             encodings["input_ids"] = F.pad(encodings["input_ids"], (0, num_padding_tokens), value=pad_val)
             encodings["attention_mask"] = F.pad(encodings["attention_mask"], (0, num_padding_tokens), value=0)
             if "token_type_ids" in encodings:
@@ -38,7 +44,7 @@ class SentenceTransformer(SentenceTransformerGT):
         return encodings
 
     def warmup_and_compile(self):
-        self.forward = torch.compile(self.forward, mode="reduce-overhead")
+        self._compiled_forward = torch.compile(super().forward, mode="reduce-overhead")
         self.eval()
 
         for target_length in self._buckets:
@@ -49,7 +55,22 @@ class SentenceTransformer(SentenceTransformerGT):
             # For BERT: [CLS]...[SEP]
             text = "a " * num_words
 
-            try:
-                _ = self.encode(text)
-            except Exception as e:
-                print(f"Bucket failed {target_length=}: {e}")
+            # Check correctness here to avoid silent performance regressions.
+            # There are other approaches like creating the encoding ourselves, padding to the target length, and calling
+            # .forward() (under inference_mode) ourselves. This approach didn't perform well—maybe b/c of subtle
+            # differences in how .encode works. I prefer going through .encode and being loud about missing the target.
+            if self.tokenize([text])["input_ids"].shape[1] != target_length:
+                raise ValueError(f"Tokenization failed for {target_length=}")
+
+            _ = self.encode(text)
+
+    def forward(self, input: dict[str, torch.Tensor], **kwargs) -> dict[str, torch.Tensor]:
+        """
+        Only use the compiled forward if the sequence length matches one of our buckets. If we used the compiled forward
+        for one that doesn't hit the bucket, we create a new CUDA graph for every unique sequence length above
+        2048—thrashing the cache.
+        """
+        seq_length = input["input_ids"].shape[1]
+        if seq_length in self._buckets:
+            return self._compiled_forward(input)
+        return super().forward(input)
