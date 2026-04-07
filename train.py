a3a1ef0bb80c1d3f3dfec0d1ba561702097e5c39
Kush Dubey <kush.dubey@sentry.io>
2026-04-06 21:44:12 -0700

    use default train paths

--- train.py
+++ train.py
@@ -78,11 +78,6 @@ def run(mini_cpu_test: bool = False):
             run_shortname="cpu-sanity-check",
             per_device_train_batch_size=2,
             per_device_token_budget=64,
-            training_csvs=(
-                "final_csvs/train.csv",
-                "final_csvs/synthetic-semi-easy-negatives.csv",
-                "final_csvs/train_more.csv",
-            ),
             gradient_checkpointing=True,
             sample_size_train=30,
             num_logs=30,
@@ -94,11 +89,6 @@ def run(mini_cpu_test: bool = False):
             per_device_train_batch_size=256,
             per_device_token_budget=8192 * 6,
             log_of_scale_init=math.log(10),  # TODO: wandb this param and bias
-            training_csvs=(
-                "final_csvs/train.csv",
-                "final_csvs/synthetic-semi-easy-negatives.csv",
-                "final_csvs/train_more.csv",
-            ),
         )
 
     trainer = gt.train.make_trainer(model, config)
