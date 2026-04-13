b75264850b2010676cbbe65d1e6a0b2adf193ba1
Kush Dubey <kush.dubey@sentry.io>
2026-04-12 18:23:42 -0700

    init not sort

--- src/grouping_trainer/__init__.py
+++ src/grouping_trainer/__init__.py
@@ -4,7 +4,14 @@ Trainer for Sentry AI grouping
 
 __version__ = "0.1.0"
 
-from . import compiled, data, evaluator, logging, loss, sentinels, train, utils
+from . import logging  # noqa: I001
+from . import sentinels
+from . import utils
+from . import compiled
+from . import data
+from . import loss
+from . import train
+from . import evaluator
 
 __all__ = [
     "__version__",
