45ba0eea90b676384f6ec878957df45df222cf1e
Kush Dubey <kush.dubey@sentry.io>
2026-06-05 01:32:38 -0700

    experiment: synthetic-hard-neg

--- train.py
+++ train.py
@@ -138,7 +138,7 @@ def run(
             learning_rate=learning_rate,
             loss_type="contrastive",
             contrastive_margin=0.5,
-            training_csvs=gt.data.DEFAULT_TRAIN_PATHS,
+            training_csvs=gt.data.DEFAULT_TRAIN_PATHS + ("final_csvs/synthetic-hard-negatives-llm.csv",),
         )
 
     gt.data.ensure_local(training_config.training_csvs)
