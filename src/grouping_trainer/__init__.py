b8351af1786032dd28c4fb3b0bb2d5bc599ab6a2
Kush Dubey <kush.dubey@sentry.io>
2026-06-07 02:49:29 -0700

    reorder imports

--- src/grouping_trainer/__init__.py
+++ src/grouping_trainer/__init__.py
@@ -7,11 +7,11 @@ __version__ = "0.1.0"
 from . import logging  # noqa: I001
 from . import sentinels
 from . import utils
+from . import compiled
+from . import data
 from . import loss
 from . import resume
 from . import launch
-from . import compiled
-from . import data
 from . import train
 from . import pretrain
 from . import evaluator
@@ -21,11 +21,11 @@ __all__ = [
     "logging",
     "sentinels",
     "utils",
+    "compiled",
+    "data",
     "loss",
     "resume",
     "launch",
-    "compiled",
-    "data",
     "train",
     "pretrain",
     "evaluator",
