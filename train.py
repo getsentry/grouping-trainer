773ef683e5c0e7699b84e7da5e99a5251a8e2a6f
Kush Dubey <kush.dubey@sentry.io>
2026-06-10 17:07:10 -0700

    experiment: harrier-no-synth

--- train.py
+++ train.py
@@ -139,7 +139,7 @@ def run(
             learning_rate=learning_rate,
             loss_type="contrastive",
             contrastive_margin=0.5,
-            training_csvs=gt.data.DEFAULT_TRAIN_PATHS,
+            training_csvs=gt.data.DEFAULT_TRAIN_PATHS_NO_SYNTHETIC,
         )
 
     gt.data.ensure_local(training_config.training_csvs)
